0fdf118b9f7b9e62e7aeae220e8e0fd3f7b68ba9
Clark Taylor <clark.n.taylor@gmail.com>
2024-12-09 11:00:12 -0500

    Update OrbitFG.py to fix bug in issue 2
    Not yet tested...

--- OrbitFG.py
+++ OrbitFG.py
@@ -159,7 +159,7 @@ class satelliteModelBatch:
             dist = la.norm(curr_state[:2])
             k = self.prop_dt*self.GE/(dist**5)
 
-            t_mat = np.array([[-self.prop_dt*(y**2-2*x**2)/2, 3*x*y*self.prop_dt, 0, 0],
+            t_mat = np.array([[-self.prop_dt*(y**2-2*x**2)/2, 3*x*y*self.prop_dt/2.0, 0, 0],
                               [3*x*y*self.prop_dt/2.0, -self.prop_dt*(x**2-2*y**2)/2, 0, 0],
                               [2*x**2-y**2, 3*x*y, 0, 0],
                               [3*x*y, 2*y**2-x**2, 0, 0]])
